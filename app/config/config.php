3114aa3b08f75df1ffbe833f164bc291990353cc
cwei <weiwu0937@gmail.com>
2021-11-08 09:18:44 +0800

    first upload

---
+++ app/config/config.php
@@ -0,0 +1,19 @@
+<?php
+  // DB Params
+  define('DB_HOST', 'localhost');
+  define('DB_USER', 'root');
+  define('DB_PASS', '123456');
+  define('DB_NAME', 'postboard');
+
+  // App Root
+  define('APPROOT', dirname(dirname(__FILE__)));
+  // URL Root
+  define('URLROOT', 'http://localhost/postboard');
+  // Site Name
+  define('SITENAME', 'Postboard');
+  // App Version
+  define('APPVERSION', '1.0.0');
+
+
+
+  
---
+++ app/controllers/Pages.php
@@ -0,0 +1,32 @@
+<?php
+  class Pages extends Controller {
+    public function __construct(){
+
+    }
+
+    public function index(){
+      if(isLoggedIn()){
+        redirect('posts');
+      }
+
+      $data = [
+        'title' => 'Postboard',
+        'description' => 'Simple postboard built on custom MVC PHP framework'
+      ];
+
+      $this->view('pages/index', $data);
+    }
+
+    public function about(){
+      $data = [
+        'title' => 'About Us',
+        'description' => 'App to share posts with other users'
+      ];
+
+      $this->view('pages/about', $data);
+    }
+  }
+
+
+
+  
